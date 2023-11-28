8e404095a4ae01b55e51bff34d47611580a39b86
JJBai2003 <jiayibai2003@gmail.com>
2023-11-27 22:32:08 -0500

    Edited app.py: - Added a class for the db structure - Added get endpoints for get all, get by name and get by id

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, request
+
+# Not too sure if this should be here or in db.py
+from flask_restful import Resource, Api
+
+
 from db import db
 import email_automater
 import json
@@ -14,6 +19,17 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
+# Not too sure if this should be here; also open to change based on the db's finilized structure 
+class EmailModel(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable=False)
+    emailAddress = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"EmailModel(name = {name}, emailAddress = {emailAddress})"
+
+
+
 def success_response(data, code = 200):
     """
     Generalized success response function
@@ -26,16 +42,35 @@ def failure_response(data, code = 404):
     """
     return json.dumps({"error" : data}), code
 
-def get_automatic_email_response():
+# Assume that we want all the email responses
+def get_all_automatic_email_response():
     """
     Retrieves and returns pre-written email response written by practitioner
     """
+    result = EmailModel.query.all()
+    return result
 
+# filter response by name
+def get_automatic_email_response_by_name(userName):
+    """
+    Retrieves and returns pre-written email response written by practitioner
+    """
+    result = EmailModel.query.filter_by(name = userName)
+    return result
+
+# filter response by id
+def get_automatic_email_response_by_name(userId):
+    """
+    Retrieves and returns pre-written email response written by practitioner
+    """
+    result = EmailModel.query.filter_by(id = userId)
+    return result
 
 def send_automatic_email_response():
     """
     Sends pre-written email response written by practitioner to client
     """
+
 
 
 if __name__ == "__main__":
