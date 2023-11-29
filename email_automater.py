19b0d8b2b063283c7a0a1e7b0728f467c5ad4960
Jephthah Mensah <99607903+wesucceed@users.noreply.github.com>
2023-11-28 20:15:30 -0500

    Update email_automater.py
    prototype for email sending

--- email_automater.py
+++ email_automater.py
@@ -1,13 +1,20 @@
-#jephthah-mensah@email-sender-405509.iam.gserviceaccount.com
-from google.oauth2 import service_account
-from google_auth_httplib2 import httplib2
+# #jephthah-mensah@email-sender-405509.iam.gserviceaccount.com
+# from google.oauth2 import service_account
+# from google_auth_httplib2 import httplib2
 
-SCOPES = ['https://www.googleapis.com/auth/sqlservice.admin', 'https://mail.google.com']
-SERVICE_ACCOUNT_FILE = './client_secret.json'
+# SCOPES = ['https://www.googleapis.com/auth/sqlservice.admin', 'https://mail.google.com']
+# SERVICE_ACCOUNT_FILE = './client_secret.json'
 
-credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+# credentials = service_account.Credentials.from_service_account_file(
+#         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 
 #TODO: looking at how to send and receive email using the api
 #TODO: read https://developers.google.com/gmail/api/reference/rest
+
+import dao.py
+# dao.get_email_by(email_id): gives a str
+# dao.get_practitioner_by_id(practitioner_id): gives the practitioner
+# dao.get_client_by_id(client_id): gives the client
+
+# send the email: returns (True, message) or (False, message)
