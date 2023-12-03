c7a7940b017ed1440bd346e01a6d9817e2685930
JJBai2003 <jiayibai2003@gmail.com>
2023-12-02 19:52:29 -0500

    - Created a SendEmail class - Wrote a method to send email from practioner's email to client's email

--- email_automater.py
+++ email_automater.py
@@ -17,4 +17,42 @@ import dao.py
 # dao.get_practitioner_by_id(practitioner_id): gives the practitioner
 # dao.get_client_by_id(client_id): gives the client
 
-# send the email: returns (True, message) or (False, message)
+
+
+
+
+# Create an object and send the email
+class SendEmail:
+    def __init__(self, pEmail, cEmail, content):
+        self.pEmail = pEmail
+        self.cEmail = cEmail
+        self.content = content
+
+        
+ # send the email: returns (True, message) or (False, message)
+import smtplib, ssl
+def send_email(pEmail, cEmail, content):
+    
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = pEmail  # Enter your address
+    receiver_email = cEmail  # Enter receiver address
+    password = input("Type your password and press enter: ")
+    message = """\{content}."""
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+
+ 
+
+
+
+    
+   
+    
+
+
+
